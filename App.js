4e14555020d6ec0205f46535a1484494108ba91f
EamaniManasa <147276480+EamaniManasa@users.noreply.github.com>
2023-10-10 23:55:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import DataDisplay from './DataDisplay';
+
+const App = () => {
+  return (
+    <div>
+      <DataDisplay />
+    </div>
+  );
+};
+
+export default App;
